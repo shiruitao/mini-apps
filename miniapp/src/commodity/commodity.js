65d0e2cecba5da4ed6c88a8a255b9486125fdf37
shitime <1257201113@qq.com>
2017-10-21 17:35:51 +0800

    看这里

--- miniapp/src/commodity/commodity.js
+++ miniapp/src/commodity/commodity.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { image: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2819681726,1592298688&fm=27&gp=0.jpg', title: 'Dell/戴尔 intel G4400 整机品牌商用办公电脑', price: 3, num: 1, selected: true },
+    { image: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2819681726,1592298688&fm=27&gp=0.jpg', title: 'Dell/戴尔', price: 3, num: 1, selected: true },
     { image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508474566792&di=159c5a9770ad94db26e68c7b8f635c39&imgtype=0&src=http%3A%2F%2Fp2.gexing.com%2Fshaitu%2F20130131%2F1022%2F5109d54d18eb6.jpg', title: '整机品牌商用办公电脑', price: 3500, num: 1, selected: true },
     { image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1508474651825&di=b057da42b99569e9f66e4109e953254b&imgtype=0&src=http%3A%2F%2Fwww.feizl.com%2Fupload2007%2Fallimg%2F170704%2F1516305H1-2.jpg', title: '整机品牌商用办公电脑', price: 3600, num: 1, selected: true }
 ]
